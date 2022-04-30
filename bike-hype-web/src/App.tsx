f17b9711593b524bf9912a896a5dfbba8d898e39
Ruta9 <rutule9@gmail.com>
2022-04-30 15:41:04 +0300

    set up sass

--- bike-hype-web/src/App.tsx
+++ bike-hype-web/src/App.tsx
@@ -1,9 +1,9 @@
-import './styles.css'
+import './styles.scss'
 import IMAGE from './bike.png'
 
 export const App = () => {
     return <>
-    <h1>Bike Hype</h1>
+    <h1 className="title">Bike Hype</h1>
     <img src={IMAGE} alt="bike" width="300"/>
     </>
 }
