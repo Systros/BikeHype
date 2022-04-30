98a4e57be948ae6d66d031bb19d7a00f08c70ce9
Ruta9 <rutule9@gmail.com>
2022-04-30 15:55:50 +0300

    set up bulma

--- bike-hype-web/src/App.tsx
+++ bike-hype-web/src/App.tsx
@@ -1,9 +1,13 @@
-import './styles.scss'
+import './sass/styles.scss'
 import IMAGE from './bike.png'
 
 export const App = () => {
     return <>
     <h1 className="title">Bike Hype</h1>
+    <div className="buttons">
+        <a className="button is-primary">Primary</a>
+        <a className="button is-link">Link</a>
+      </div>
     <img src={IMAGE} alt="bike" width="300"/>
     </>
 }
